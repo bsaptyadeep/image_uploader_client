4786b0579e784fa7f44997547e95f6fac0539dba
Saptyadeep <bsaptyadeep@gmail.com>
2022-07-15 19:27:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DisplayImage from './components/displayImage';
+import UploadImage from './components/uploadImage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#upload">Image Uploader</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#upload">Upload Image</Nav.Link>
+            <Nav.Link href="#view">View</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div id='upload'>
+        <UploadImage/>
+      </div>
+      <div id='view'>
+        <DisplayImage/>
+      </div>
     </div>
   );
 }
